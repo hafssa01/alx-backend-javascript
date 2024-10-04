b463f001cf32465dbcd6577f4d8ac98d7b43c8e0
hafssa01 <hafssazaferr@gmail.com>
2024-10-04 23:36:20 +0100

    the rest...

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    this.throwError('Class extending Building must override evacuationWarningMessage');
-  }
-
-  throwError(message) {
-    throw new Error(message);
   }
 }
