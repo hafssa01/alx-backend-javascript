b8b40d58331a6706ed1ab8007782d678d9b7e958
hafssa01 <hafssazaferr@gmail.com>
2024-10-11 15:25:02 +0100

    map(grociers)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const grociers = new Map();
+
+  grociers.set('Apples', 10);
+  grociers.set('Tomatoes', 10);
+  grociers.set('Pasta', 1);
+  grociers.set('Rice', 1);
+  grociers.set('Banana', 5);
+
+  return grociers;
+}
